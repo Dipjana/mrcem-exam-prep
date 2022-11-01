9f5dd88194e2e287728accb8c11bd19a0f9d0507
Dipjana <dipsundarjana@gmail.com>
2022-11-01 12:59:13 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'contact-us',
+    loadChildren: () => import('./pages/contact-us/contact-us.module').then(m => m.ContactUsModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AUTH_STATE_NAME } from 'src/app/store/auth/auth.selector';
+import { AuthReducer } from 'src/app/store/auth/auth.reducer';
+import { StoreModule } from '@ngrx/store';
+ 
+@NgModule({
+  declarations: [
+    LoginComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forChild([
+      {path: '', component: DashboardComponent},
+      {path: 'login', component: LoginComponent}
+    ]),
+    StoreModule.forFeature(AUTH_STATE_NAME, AuthReducer)
+  ]
+})
+export class DashboardModule { }
